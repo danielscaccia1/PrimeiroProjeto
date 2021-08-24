687ab5c19ebcd111b10b1c73d95753e14d171756
danielscaccia1 <daniel.scaccia@asaconsultoria.com.br>
2021-08-24 11:51:25 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SalesWebMvc.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
